ae056977b80728c836ae714e46987421d140735d
Tomáš Neuschl <tm.neu003@gmail.com>
2024-03-26 14:53:49 +0100

    add django models and init migration

--- flight_data/models/flight.py
+++ flight_data/models/flight.py
@@ -2,7 +2,20 @@ from django.db import models
 
 from flight_analysis.settings import CHARFIELD_MAX_LENGTH
 from flight_data.models.base import BaseModel
+from flight_data.models.status import Status
+from flight_data.models.telemetry import Telemetry
 
 
 class Flight(BaseModel):
     name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            super().save(*args, **kwargs)
+            Telemetry.objects.create(flight=self)
+            Status.objects.create(flight=self)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "flight"
+        verbose_name_plural = "flights"
--- flight_data/models/telemetry.py
+++ flight_data/models/telemetry.py
@@ -1,9 +1,26 @@
 from django.db import models
 
-from flight_analysis.settings import CHARFIELD_MAX_LENGTH
+from flight_analysis.settings import CHARFIELD_MAX_LENGTH, INFLUXDB_HOST, INFLUXDB_PORT, \
+    INFLUXDB_DATABASE
 from flight_data.models.base import BaseModel
+from flight_data.services.Influx_service import InfluxService
 
 
 class Telemetry(BaseModel):
-    csv_name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
-    flight = models.ForeignKey("Flight", on_delete=models.CASCADE)
+    csv_name = models.CharField(max_length=CHARFIELD_MAX_LENGTH,
+                                verbose_name="File name",
+                                null=True)
+    flight = models.OneToOneField("Flight", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "telemetry"
+        verbose_name_plural = "telemetry"
+
+    def delete(self, *args, **kwargs):
+        self.csv_name = None
+        InfluxService(
+            host=INFLUXDB_HOST,
+            port=INFLUXDB_PORT,
+            database=INFLUXDB_DATABASE,
+        ).delete_data("telemetry_id", self.id)
+        self.save()
